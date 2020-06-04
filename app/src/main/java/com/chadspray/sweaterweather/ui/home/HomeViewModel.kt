461fa9fa21c2284c793423825e209b9d215a5ad0
Chad <chdspray@gmail.com>
2020-06-04 07:02:58 -0500

    Retrieves zipcode and in turn the weather

--- app/src/main/java/com/chadspray/sweaterweather/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/chadspray/sweaterweather/ui/home/HomeViewModel.kt
@@ -1,17 +1,32 @@
 package com.chadspray.sweaterweather.ui.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import WeatherDTO
+import android.location.Geocoder
+import androidx.lifecycle.*
+import com.chadspray.sweaterweather.service.SweaterResponse
+import com.chadspray.sweaterweather.ui.weather.WeatherViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import java.lang.Exception
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel : WeatherViewModel() {
 
-    private val _hasLocationPermission = MutableLiveData<Boolean>().apply {
-        value = false
+    private val _zipcode = MutableLiveData<String>()
+
+    private fun getZipcode(fusedLocationClient: FusedLocationProviderClient, geocoder: Geocoder) : LiveData<String> {
+        fusedLocationClient.lastLocation.addOnSuccessListener {location ->
+            try {
+                _zipcode.postValue(geocoder
+                    .getFromLocation(location.latitude, location.longitude, 1)[0]
+                    .postalCode)
+            } catch (ignored: Exception) {
+
+            }
+        }
+        return _zipcode
     }
 
-    val hasLocationPermission: LiveData<Boolean> = _hasLocationPermission
-
-    fun setLocationPermissionGranted() = _hasLocationPermission.postValue(true)
+    fun getLocalWeather(fusedLocationClient: FusedLocationProviderClient, geocoder: Geocoder): LiveData<SweaterResponse<WeatherDTO>> {
+        return Transformations.switchMap(getZipcode(fusedLocationClient, geocoder), this::getWeatherData)
+    }
 
 }
